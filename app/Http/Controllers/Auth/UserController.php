6f82ca5419bbfbf90bc5fcededfaa416c86d9317
celxkodez <decele2011@gmail.com>
2020-09-24 01:47:59 +0100

    user controller

---
+++ app/Http/Controllers/Auth/UserController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Auth;
+use App\Http\Controllers\Controller;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+
+    public function register()
+    {
+        $this->validate(request(), [
+            'email' => ['required','email', 'unique:users'],
+            'password' => ['required', 'confirmed'],
+            'name' => ['required','string']
+        ]);
+
+        $data = request(['email', 'password', 'name']);
+
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+
+        $token = $user->createToken('auth_sys');
+        $accessToken = $token->accessToken;
+        $res = [
+            'status' => true,
+            'message' => 'successful',
+            'code' => 200,
+            'user' => $user,
+            'token' => [
+                'access_token' => $accessToken,
+                'token_type' => 'Bearer',
+                'expires_at' => Carbon::parse(
+                    $token->token->expires_at
+                )->toDateTimeString()
+            ]
+        ];
+
+        return response()->json($res);
+    }
+    
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+
+        if (!Auth::attempt($credentials)) {
+
+            $res = [
+                'status' => false,
+                'code' => 401,
+                'message' => 'Unauthorized',
+                'description' => 'Wrong credentials'
+            ];
+            return response()->json($res);
+        }
+
+        $user = request()->user();
+        $token = $user->createToken('Personal Access Token');
+        $accessToken = $token->accessToken;
+
+        $res = [
+            'status' => true,
+            'code' => 200,
+            'message' => 'Success',
+            'user' => $user,
+            'token' => [
+                'access_token' => $accessToken,
+                'token_type' => 'Bearer',
+                'expires_at' => Carbon::parse(
+                    $token->token->expires_at
+                )->toDateTimeString()
+            ]
+        ];
+
+        return response()->json($res);
+    }
+}
